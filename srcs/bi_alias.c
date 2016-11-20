cc4f5ae8dc64f862f5765a3bd91093d1eb691f1b
Ilyes Bouchlaghem <ilyesbouchlaghem@gmail.com>
2016-11-20 02:14:09 +0100

    updating alias bultin

--- srcs/bi_alias.c
+++ srcs/bi_alias.c
@@ -12,12 +12,15 @@
 
 typedef struct		s_alias
 {
-	char			*name;
+	char			*key;
 	char			*value;
 	struct s_alias	*next;
 }					t_alias;
 
 /*
+
+ LA SUBSTITUTION DE L'ALIAS N'EST EFFECTUER QU'AU MOMENT DE L'EXECUTION, C'EST D'AILLEUR LA PREMIERE ETAPE
+
  **	alias | print la liste des alias
  **	alias ls | print la valeure de ls ou si il n'existe pas print msg d'erreur code retour >0
  ** alias name=value | comportement classique ou meme 'name=' deviendra alias name=""
@@ -30,23 +33,106 @@ typedef struct		s_alias
  Si il y a une boucle infinie type a->b; b->c; c->a; alors aucun remplacement sur l'alias n'est effectue.
  Sinon tant que il y a encore un alias 'name' qui vaut la value de l'alias donner en arg on continue, puis on remplace l'alias par la value
 
-
-
 */
 
-int	bi_alias(void)
+int	print_alias_key(t_alias *alias, char *key)
 {
-	(void)g_env;
-	(void)av;
-	(void)l_env;
-	ft_putstr("ON RENNNNNNNNNNNTRE");
+	while (alias != NULL)
+	{
+		if ((ft_strcmp(alias->key, key)) == 0)
+		{
+			ft_putstr(alias->key);
+			ft_putchar('=');
+			ft_putendl(alias->value);		
+			return (0);
+		}
+		alias = alias->next;
+	}
+	return (1);	
+}
+
+int	array_key_exists(t_alias *alias, char *key)
+{
+	while (alias != NULL)
+	{
+		if ((ft_strcmp(alias->key, key)) == 0)
+			return (1);
+		alias = alias->next;
+	}
 	return (0);
 }
 
-l = ls -laH
-zz = l;
+int	print_alias_list(t_alias *alias, char *key)
+{
+	if (key != NULL)
+		return ((array_key_exists(alias, key)) ? print_alias_key(alias, key) : 1);
+	while (alias != NULL)
+	{
+		ft_putstr(alias->key);
+		ft_putchar('=');
+		ft_putendl(alias->value);
+		alias = alias->next;
+	}
+	return (0);
+}
+
+char	*strdup_aplha(char *cmd)
+{
+	int		i;
+	char	*ret;
+
+	i = 1;
+	if (cmd == '\0')
+		return(ft_strdup("\0"));
+	while ((ft_isalpha(cmd[i])) && cmd[i] != '\0')
+		++i;
+	if (!(ret = ft_strnew(i)))
+		return ((char *)NULL);
+	i = 1;
+	while ((ft_isalpha(cmd[i])) && cmd[i] != '\0')
+	{
+		ret[i - 1] = cmd[i];
+		++i;
+	}
+	ret[i - 1] = '\0';
+	return (ret);
+}
+
+char	**get_data(char *cmd)
+{
+	char	**pair;
+
+	if (!(pair = (char **)ft_memalloc(sizeof(char *) * 2)))
+		return ((char **)NULL);
+	while ((ft_isspace(cmd[i])))
+		++i;
+	pair[0] = strdup_aplha((cmd + i));
+	pair[1] = ((cmd[i] == '=') ? NULL : strdup_aplha((cmd + i)));
+	return (pair);
+}
+
+int	bi_alias(char *cmd, int argc)
+{
+	int		x;
+	char	**pair;
+
+	x = 0;
+	if (argc == 1)
+		return (print_alias_list((char *)NULL));
+	while (arg[++x] != NULL)
+	{
+		if ((pair = get_data(cmd)))
+		{
+			if (pair[1] == NULL)
+				if ((print_alias_list(pair[0])) != 0)
+					ft_putendl_fd("21sh: Alias not found", 2);
+			del_pairs();
+		}
+	}
+	return (0);
+}
 
 int	main(void)
 {
-	bi_alias("alias l=i")
+	bi_alias("alias l=i", 2)
 }
