dc4ff8ad70bcb019668854bf12daad6cfa78790b
Ilyes BOUCHLAGHEM <ibouchla@student.42.fr>
2016-12-02 20:30:14 +0100

    add hash functions

---
+++ hash_dir/hash.c
@@ -0,0 +1,136 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hash.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibouchla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/02 17:10:59 by ibouchla          #+#    #+#             */
+/*   Updated: 2016/12/02 17:11:02 by ibouchla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/* A mettre dans un .h */
+typedef struct		s_hash
+{
+	unsigned int	hash;
+	char			*bin;
+	char			*path;
+	struct s_hash	*next;
+}					t_hash;
+
+void			init_bin_list(t_hash **addr, char *path);
+unsigned int	hash_algo(char *key, int nb);
+void			hash_del(t_hash ***hash, unsigned int hash_size);
+char			*get_hash_path(t_hash **hash, char *bin, unsigned int g_hash_size);
+int				collision = 0;
+unsigned int	g_hash_size = 0;
+/* A mettre dans un .h */
+
+static unsigned int	list_size(t_hash *lst)
+{
+	unsigned int ret;
+
+	ret = 0;
+	while (lst != NULL)
+	{
+		lst = lst->next;
+		++ret;
+	}
+	return (ret);
+}
+
+static t_hash		*elem_list_dup(t_hash *master)
+{
+	t_hash	*new_node;
+
+	if (!(new_node = (t_hash *)ft_memalloc(sizeof(t_hash))))
+		return (NULL);
+	new_node->bin =  ((master->bin != NULL) ? ft_strdup(master->bin) : NULL);
+	new_node->path = ((master->path != NULL) ? ft_strdup(master->path) : NULL);
+	new_node->next = NULL;
+	return (new_node);
+}
+
+static void			insert_hash(t_hash ***hash, t_hash *lst, unsigned int nb)
+{
+	unsigned int	id;
+	t_hash			*new_node;
+
+	id = hash_algo(lst->bin, nb);
+	if (!(new_node = elem_list_dup(lst)))
+		return ;
+	if ((*hash)[id] != NULL)
+	{
+		++collision;
+		new_node->next = (*hash)[id]->next;
+		(*hash)[id]->next = new_node;	
+	}
+	else
+		(*hash)[id] = new_node;
+}
+
+static void			lst_del(t_hash **addr)
+{
+	t_hash	*tmp;
+
+	while (*addr)
+	{
+		if (((*addr)->bin) != NULL)
+			ft_strdel(&((*addr)->bin));
+		if (((*addr)->path) != NULL)
+			ft_strdel(&((*addr)->path));
+		tmp = *addr;
+		*addr = (*addr)->next;
+		if (tmp != NULL)
+			ft_memdel((void **)&tmp);
+	}
+}
+
+static t_hash		**hash_table(char *path)
+{
+	t_hash			*lst;
+	t_hash			**hash;
+	t_hash			*tmp;
+
+	lst = NULL;
+	init_bin_list(&lst, path);
+	g_hash_size = list_size(lst);
+	if (!(hash = (t_hash **)ft_memalloc(sizeof(t_hash *) * g_hash_size)))
+		return (NULL);
+	tmp = lst;
+	while (tmp != NULL)
+	{
+		insert_hash(&hash, tmp, g_hash_size);
+		tmp = tmp->next;
+	}
+	lst_del(&lst);
+	return (hash);
+}
+
+int	main(void)
+{
+	t_hash	**hash;
+	char	*p;
+	char	*b = "sub";
+
+	p = "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/munki:/Users/ibouchla/bin";
+	hash = hash_table(p);
+	printf("[%d] Path : %s\n", hash_algo(b, g_hash_size), get_hash_path(hash, b, g_hash_size));
+	hash_del(&hash, g_hash_size);
+	printf("Nombre de collisions : %d\n", collision);
+	/*for (int id = 0; id < g_hash_size; id++)
+	{
+		if (hash[id] != NULL)
+			for (int x = 0; (hash[id]); x++) 
+			{
+				for (int z = 0; z < x; z++)
+					ft_putstr("\t_____");
+				printf("[%d] [bin] -> %s [path] -> : %s\n", id, hash[id]->bin, hash[id]->path);
+				hash[id] = hash[id]->next;
+			}
+	}
+	*/
+}
---
+++ hash_dir/init_bin_list.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_bin_list.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibouchla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/02 20:04:07 by ibouchla          #+#    #+#             */
+/*   Updated: 2016/12/02 20:04:11 by ibouchla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <dirent.h>
+#include <sys/stat.h>
+
+/* A mettre dans un .h */
+typedef struct		s_hash
+{
+	long int		hash;
+	char			*bin;
+	char			*path;
+	struct s_hash	*next;
+}					t_hash;
+/* A mettre dans un .h */
+
+static char		*storage_path(char *path, char *name)
+{
+	size_t	end;
+	char	*path_file;
+	char	*tmp;
+
+	if (!path || !name)
+		return (NULL);
+	end = (ft_strlen(path) - 1);
+	if (path[end] == '/')
+		path_file = ft_strjoin(path, name);
+	else
+	{
+		if ((tmp = ft_strjoin(path, "/")))
+		{
+			path_file = ft_strjoin(tmp, name);
+			ft_strdel(&tmp);
+		}
+	}
+	return (path_file);
+}
+
+static t_hash	*add_new_node(char *bin, char *path)
+{
+	t_hash	*new_node;
+
+	if (!(new_node = (t_hash *)ft_memalloc(sizeof(t_hash))))
+		return (NULL);
+	new_node->bin = ((bin != NULL) ? ft_strdup(bin) : NULL);
+	new_node->path = ((path != NULL) ? ft_strdup(path) : NULL);
+	new_node->next = NULL;
+	return (new_node);
+}
+
+static void		get_dir_elem(t_hash **addr, DIR *directory, char *path)
+{
+	t_hash			*begin;
+	t_hash			*new_node;
+	struct dirent	*elem;
+	char			*new_path;
+
+	while ((elem = readdir(directory)) != NULL)
+	{
+		if (elem->d_name[0] == '.')
+			continue ;
+		if (!(new_path = storage_path(path, elem->d_name)))
+			return ;
+		if ((access(new_path, X_OK)) == 0)
+		{
+			if (!(new_node = add_new_node(elem->d_name, new_path)))
+				return ;
+			if (*addr != NULL)
+			{
+				new_node->next = *addr;
+				*addr = new_node;
+			}
+			else
+				*addr = new_node;
+		}
+		ft_strdel(&(new_path));
+	}
+}
+
+void			init_bin_list(t_hash **addr, char *path)
+{
+	char		**all_path;
+	int			i;
+	struct stat	st;
+	DIR			*directory;
+
+	if ((all_path = ft_strsplit(path, ':')) == NULL)
+		return ;
+	i = (-1);
+	while (all_path[++i] != NULL)
+		if ((access(all_path[i], F_OK)) == 0)
+			if ((stat(all_path[i], &st)) == 0)
+				if ((S_ISDIR(st.st_mode)))
+					if ((access(all_path[i], X_OK)) == 0)	
+						if ((directory = opendir(all_path[i])) != NULL)
+						{
+							get_dir_elem(addr, directory, all_path[i]);
+							closedir(directory);
+						}
+	ft_tabdel(all_path);
+}
