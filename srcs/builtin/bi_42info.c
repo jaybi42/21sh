8593469e78ac43a8e2989cd1c07404ed830db4ad
Ilyes BOUCHLAGHEM <ibouchla@student.42.fr>
2016-12-04 22:06:51 +0100

    update 42info

--- srcs/builtin/bi_42info.c
+++ srcs/builtin/bi_42info.c
@@ -2,53 +2,72 @@
 #include <stdlib.h>
 
 /*
-** - clean screen
-*/
+ ** - clean screen
+ */
 typedef struct s_info
 {
-  char *name;
-  char *info;
+	char *name;
+	char *info;
 }             t_info;
 /*
-** !!!!!!! DON'T ADD: %s in your info or any %*
-*/
-#define LEN_INFOS 1
+ ** !!!!!!! DON'T ADD: %s in your info or any %*
+ */
+#define LEN_INFOS 2
 
 t_info g_infos[LEN_INFOS] = {
-{"|",
-"{green}#definition{eoc}\n\
-\'|\' is called  a \'pipe\'. it's a specific separator between two command,\n\
-the format is: \"cmd1 | cmd2\"\n\
-\n\
-{green}#how it works?{eoc}\n\
-the output (stdout{red}(?){eoc}) of the cmd1 will be put in the stdin\
-{red}(?){eoc} of the cmd2.\n\
-\n\
-{green}#examples{eoc}\n\
-$>echo \"ok\" | cat -e <ENTER>\n\
-ok$\n\
-"
-}
+	{"|",
+		"{green}#Definition :{eoc}\n\
+			\'|\' is called  a \'pipe\'. it's a specific separator between two commands,\n\
+			The format is: \"command1 | command2\"\n\
+			\n\
+			{green}#How it works ?{eoc}\n\
+		The output (stdout{red}(?){eoc}) of the command1 will be put in the stdin\
+		{red}(?){eoc} of the command2.\n\
+		\n\
+		{green}#Examples :{eoc}\n\
+		$>echo \"ok\" | cat -e <ENTER>\n\
+			ok$\n\
+			"},
+		{"alias",
+			"{green}#Definition :{eoc}\n\
+				Alias system is a implementation of command\'s substitution,\n\
+				The format is: \"{green}key{eoc}={red}value{eoc}\"\n\
+				\n\
+				{green}#How it works ?{eoc}\n\
+			When you send a command to the shell, if it\'s present in the alias\'s list,\n\
+				it\'s search recursively the {red}value{eoc} associated to the {green}key{eoc}.\n\
+				When an infinite loop is detected, no substitution is made.\n\
+				\n\
+				{green}#Examples :{eoc}\n\
+			$> alias \"{green}key1{eoc}={red}value1{eoc}\"\n\
+				$> alias \"{green}value1{eoc}={red}ls -la -h{eoc}\"\n\
+				$> key1\n\
+				total 0\n\
+				drwxr-xr-x   4 ibouchla  2015_paris  136 Dec  4 20:41 .\n\
+				drwxr-xr-x  13 ibouchla  2015_paris  442 Dec  4 20:41 ..\n\
+				drwxr-xr-x   2 ibouchla  2015_paris   68 Dec  4 20:41 bar\n\
+				-rw-r--r--   1 ibouchla  2015_paris    0 Dec  4 20:41 foo\n"
+		}
 };
 
-int exec_42info(int ac, char **argv)
+int	exec_42info(int ac, char **argv)
 {
-  int i;
+	int i;
 
-  if (ac == 0)
-  {
-    ft_dprintf(2, "usage: 42info \'?\'\n");
-    return (0);
-  }
-  i = -1;
-  while (++i < LEN_INFOS)
-    if (ft_strcmp(g_infos[i].name, argv[1]) == 0)
-    {
-      ft_printf(g_infos[i].info);
-      return (0);
-    }
-  ft_printf("didn't find '%s'!\n", argv[1]);
-  return (0);
+	if (ac == 0)
+	{
+		ft_dprintf(2, "Usage: 42info \'?\'\n");
+		return (0);
+	}
+	i = -1;
+	while (++i < LEN_INFOS)
+		if (ft_strcmp(g_infos[i].name, argv[1]) == 0)
+		{
+			ft_printf(g_infos[i].info);
+			return (0);
+		}
+	ft_printf("Didn't find '%s'!\n", argv[1]);
+	return (0);
 }
 
 int			bi_42info(t_av av, t_list **g_env, t_list **l_env)
