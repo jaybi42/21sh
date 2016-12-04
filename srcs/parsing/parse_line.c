c9554ee4b0ee28fb8fbe743e13c1886ab5295edc
Jean-baptiste GUTHERTZ <jguthert@e3r3p10.42.fr>
2016-12-04 18:43:47 +0100

    history newline fixed

--- srcs/parsing/parse_line.c
+++ srcs/parsing/parse_line.c
@@ -6,11 +6,22 @@
 /*   By: jguthert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/25 15:47:39 by jguthert          #+#    #+#             */
-/*   Updated: 2016/12/04 18:23:58 by jguthert         ###   ########.fr       */
+/*   Updated: 2016/12/04 18:41:17 by jguthert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "21sh.h"
+
+static void	parse_newline(char *str, t_ftl_root *hist)
+{
+	char	**ans;
+	int		i;
+
+	i = 0;
+	ans = ft_strsplit(str, '\n');
+	while (ans[i])
+		add_history(ans[i++], hist);
+}
 
 int			parse_line(t_line *l, t_ftl_root *hist)
 {
@@ -20,6 +31,6 @@ int			parse_line(t_line *l, t_ftl_root *hist)
 		return (1);
 	if (parse_quote(l) == 1)
 		return (1);
-	add_history(l->str, hist);
+	parse_newline(l->str, hist);
 	return (0);
 }
