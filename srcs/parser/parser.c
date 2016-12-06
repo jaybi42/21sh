8819610c02b0bbf3e7482fb01402e83f037baa16
Anis GADHGADHI <agadhgad@e3r1p2.42.fr>
2016-12-06 16:37:46 +0100

    update todo

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: ibouchla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/28 22:16:21 by ibouchla          #+#    #+#             */
-/*   Updated: 2016/11/17 18:20:33 by malaine          ###   ########.fr       */
+/*   Updated: 2016/12/06 16:32:21 by agadhgad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,7 +92,7 @@ void   exclude_s(t_norm_simplify *t)
 	(*t->expr) =  tmp;
 }
 
-void    simplify(char **expr, int **t_ind, int **l_ind)
+void    simplify_space(char **expr, int **t_ind, int **l_ind)
 {
 	t_norm_simplify t;
 
@@ -116,6 +116,59 @@ void    simplify(char **expr, int **t_ind, int **l_ind)
 	}
 }
 
+void	put_space_around(t_norm_simplify *t)
+{
+	int x;
+	int w;
+	char *tmp;
+	int e;
+
+	x = 0;
+	tmp = xmalloc(sizeof(char) * (ft_strlen((*t->expr)) + t->a + 4));
+	while ((*t->t_ind)[x] != -1)
+	{
+		if ((*t->t_ind)[x] >= t->i)
+			(*t->t_ind)[x] += t->a + 1;
+		x++;
+	}
+	x = -1;
+	w = 0;
+	while (++x < (int)ft_strlen((*t->expr)))
+	{
+		if (x < t->i || x > t->i)
+			tmp[w++] = (*t->expr)[x];
+		else if (x == t->i)
+		{
+			tmp[w++] = ' ';
+			e = -1;
+			while(++e < t->a)
+				tmp[w++] = (*t->expr)[x + e];
+			tmp[w++] = ' ';
+			x += e - 1;
+		}
+	}
+	tmp[w] = '\0';
+	(*t->expr) = tmp;
+	(t->i) += t->a;
+}
+
+void    simplify_connector(char **expr, int **t_ind, int **l_ind)
+{
+	t_norm_simplify t;
+
+	t.expr = expr;
+	t.t_ind = t_ind;
+	t.l_ind = l_ind;
+	t.i = 0;
+	while ((*expr)[t.i])
+	{
+		if ((t.a = is_connector((*expr), t.i, (*t_ind), (*l_ind)))
+				&& !is_intouchable(t.i, (*t.t_ind), (*t.l_ind)))
+			put_space_around(&t);
+		t.i++;
+	}
+}
+
 /*
 ** check the var from the env!
 */
@@ -124,7 +177,8 @@ t_av **nparse(char *expr, int *t_ind, int *l_ind)
 	t_av **cmds;
 	t_nparse np;
 
-	simplify(&expr, &t_ind, &l_ind);
+	simplify_connector(&expr, &t_ind, &l_ind);
+	simplify_space(&expr, &t_ind, &l_ind);
 	np = parse(expr, t_ind, l_ind);
 	if (np.failed == TRUE)
 	{
