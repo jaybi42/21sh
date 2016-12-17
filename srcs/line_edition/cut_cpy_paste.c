c72d37c96690a5fb356dc4c4e99f7dad9a220935
jaybi42 <jb.guthertz@gmail.com>
2016-12-18 00:22:45 +0100

    First try to set a big print mode

--- srcs/line_edition/cut_cpy_paste.c
+++ srcs/line_edition/cut_cpy_paste.c
@@ -13,23 +13,29 @@
 #include "edit_line.h"
 #include "shell.h"
 
-void		print(t_line *l)
+static void	print_str(t_line *l, int window)
 {
 	int a;
 
-	a = -1;
-	ft_home(l);
-	do_term("cd");
-	while (l->str[++a] != '\0')
+	a = l->start_print - 1;
+	while (l->str[++a] != '\0' && --window)
 	{
+		//On ne peut pas faire du putstr directement ??
 		ft_putchar(l->str[a]);
 		if (l->largeur != 0 && l->count != 0
 		&& (l->count + l->sizeprompt + 1) % l->largeur == 0)
 			do_term("do");
 		l->count++;
 	}
-	ft_home(l);
-	while (l->count < l->final_count)
+}
+
+static void	put_cursor(t_line *l, int window)
+{
+	//Je ne comprends pas pourquoi on va de count à final_count. Pourquoi on ne va pas de l->start_print à count ?!
+	//En sachant que pour un little_print, start_print vaut forcément 0. Comme ça on se débarasse de window par la même occasion
+	//a = l->start_print;
+	// while (a++ < l->count){lafonction;}
+	while (l->count < l->final_count && --window)
 	{
 		if (l->largeur != 0 && l->count != 0
 		&& (l->count + l->sizeprompt + 1) % l->largeur == 0)
@@ -38,6 +44,52 @@ void		print(t_line *l)
 			do_term("nd");
 		l->count++;
 	}
+}
+
+static void	big_print(t_line *l, int window)
+{
+	//Début - Fonction d'initialisation
+	//fonction ci pour avoir les 5 fonctions de print dans un même fichier...
+	if (l->start_print == -1)//Si c'est la première fois que l'on affiche le gros print
+	{
+		l->start_print = size - window;
+		l->count = l->start_print;//l->count = size si on modifie la manière de placer le curseur
+		l->final_count= size;
+	}
+	else if (l->count < l->start_print)//si on a déplacé le curseur vers le début, on met à jour start_print
+		l->start_print = count;//Voir si il faut mettre final_count à la place de count
+	else if (l->count > l->start_print + window)//Si on a déplacé le curseur vers la fin, on met à jour start_print
+		l->start_print = count - window;//Voir si il faut mettre final_count à la place de count
+	//Fin - Fonction d'initialisation
+	do_term("cl");
+	print_prompt(g_prompt.rand, g_prompt.g_env, g_lenv, g_prompt.l);
+    	if (l->start_print != 0)//On gère le cas où le début de la str a été print
+		ft_putstr(". . . .\n");
+	print_str(l, window);
+	do_goto("cm", 0, 0);
+	put_cursor(l, window);
+	if (l->start_print + window < size)//On gère le cas où la fin de la str n'a pas été print
+		ft_putstr("\n. . . .");
+}
+
+static void	little_print(t_line *l, int window)
+{
+	ft_home(l);
+	do_term("cd");
+	print_str(l, window);
+	ft_home(l);
+	put_cursor(l, window);
+}
+
+void		print(t_line *l)
+{
+	int window;
+	
+	window = (l->hauteur - 4) * l->largeur;//4 lignes pour ne pas se prendre la tête
+	if (size > window)
+		big_print(l, window);
+	else
+		little_print(l, window);
 }
 
 void		ft_cut(t_line *l)
