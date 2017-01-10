7cd8351bb4381875244b08ea156b07ab52501fc2
Jean-baptiste GUTHERTZ <jguthert@e3r3p5.42.fr>
2017-01-10 19:37:39 +0100

    env fixed

--- srcs/builtin/bi_unsetenv.c
+++ srcs/builtin/bi_unsetenv.c
@@ -6,7 +6,7 @@
 /*   By: jguthert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/20 13:29:49 by jguthert          #+#    #+#             */
-/*   Updated: 2016/12/11 17:54:46 by jguthert         ###   ########.fr       */
+/*   Updated: 2017/01/10 19:37:15 by jguthert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,25 @@ static void	unset_link(t_list **g_env, t_list *link)
 		prev = prev->next;
 	prev->next = link->next;
 	ft_lstdelone(&link, free_env);
+}
+
+static int	find_and_replace_same(t_list *link, t_av av, t_list **g_env)
+{
+	while (link != NULL)
+	{
+		if (ft_strcmp(((t_env *)link->content)->name, *av.arg) == 0)
+		{
+			if (ft_strcmp(((t_env *)link->content)->name, "PATH") == 0)
+			{
+				hash_del(&g_hash);
+				g_hash = NULL;
+			}
+			unset_link(g_env, link);
+			return (1);
+		}
+		link = link->next;
+	}
+	return (0);
 }
 
 static int	find_and_replace(t_list *link, t_av av, t_list **g_env)
@@ -62,5 +81,7 @@ int			bi_unsetenv(t_av av, t_list **g_env, t_list **g_lenv)
 		return (print_error(av, 5));
 	else if (av.argc == 1 && ft_strisprint(*av.arg) == 0)
 		return (print_error(av, 4));
+	if (find_and_replace_same(link, av, g_env) == 1)
+		return (0);
 	return (find_and_replace(link, av, g_env));
 }
